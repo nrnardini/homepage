840aeb5f6b83004ca55b4080ddabb87f7faec621
Nick <nrnardini@gmail.com>
2023-01-01 16:20:31 -0800

    Integrate tumblechess

--- tumblechess.js
+++ tumblechess.js
@@ -1,162 +1,297 @@
-const fric = 0.6;
-const rest = 0.7;
+Matter.use('matter-wrap');
 
-const {
-  Engine,
-  Runner,
-  World,
-  Bodies,
-  Body,
-  Common,
-  Composite,
-  Mouse,
-  MouseConstraint,
-  Vertices
-} = Matter;
+let characterTouchingASurface = false
+let stat = false;
+let grav = .35;
 
-const pblob = 'M 805 912 L 747 970 L 756 1034 L 959 1038 L 954 972 L 916 913 L 915 796 L 965 711 L 944 638 L 859 607 L 794 638 L 787 719 L 838 768 L 805 912 Z'
-const blob = Vertices.fromPath(pblob);
-
-// from http://paulbourke.net/geometry/polygonmesh/
-function computeArea(vertices) {
-  let area = 0;
-  for (let i = 0; i < vertices.length - 1; i++) {
-    let v = vertices[i];
-    let vn = vertices[i + 1];
-    area += (v.x * vn.y - vn.x * v.y) / 2;
-  }
-
-  return area;
-}
-
-
-
-function computeCenter(vertices) {
-  let area = computeArea(vertices);
-  let cx = 0,
-    cy = 0;
-  for (let i = 0; i < vertices.length - 1; i++) {
-    let v = vertices[i];
-    let vn = vertices[i + 1];
-    cx += (v.x + vn.x) * (v.x * vn.y - vn.x * v.y) / (6 * area);
-    cy += (v.y + vn.y) * (v.x * vn.y - vn.x * v.y) / (6 * area);
-  }
-
-  return {
-    x: cx,
-    y: cy
-  };
-}
-
-const center = computeCenter(blob);
-
-let engine;
-let world;
-let blobs = [];
-
+let board;
 let ground;
 let ceiling;
-let wallLeft;
-let wallRight;
+let lwall;
+let rwall;
 
-let mConstraint;
+let blackrook1;
+let blackknight1;
+let blackbishop1;
+let blackqueen;
+let blackking;
+let blackbishop2;
+let blackknight2;
+let blackrook2;
+let blackpawn1;
+let blackpawn2;
+let blackpawn3;
+let blackpawn4;
+let blackpawn5;
+let blackpawn6;
+let blackpawn7;
+let blackpawn8;
 
-//blob creation function
-function Blob(x, y) {
-  let options = {
-    friction: fric,
-    restitution: rest
-  }
+let whiterook1;
+let whiteknight1;
+let whitebishop1;
+let whitequeen;
+let whiteking;
+let whitebishop2;
+let whiteknight2;
+let whiterook2;
+let whitepawn1;
+let whitepawn2;
+let whitepawn3;
+let whitepawn4;
+let whitepawn5;
+let whitepawn6;
+let whitekpawn7;
+let whitepawn8;
 
-  this.body = Bodies.fromVertices(x, y, blob, options);
-  World.add(world, this.body);
-  
-  // Scales the body around the center
-  Body.scale(this.body, 0.5, 0.5);
+let blackrookSprite;
+let blackknightSprite;
+let blackbishopSprite;
+let blackqueenSprite;
+let blackkingSprite;
+let blackpawnSprite;
 
-  this.show = function() {
-    var pos = this.body.position;
-    var angle = this.body.angle;
+let whiterookSprite;
+let whiteknightSprite;
+let whitebishopSprite;
+let whitequeenSprite;
+let whitekingSprite;
+let whitepawnSprite;
 
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    scale(0.5, 0.5);
-    translate(-center.x, -center.y);
-    strokeWeight(0);
-    fill('#546B2E')
-    beginShape();
-    for (const {
-        x,
-        y
-      } of blob) {
-      curveVertex(x, y);
-    }
-    endShape(CLOSE);
-    pop();
 
-    // Alternately, when drawing your blobs you could use 
-    // the bodies vertices, but it looks like these are
-    // converted into a convex polygon.
-    push();
-    stroke('red');
-    strokeWeight(1);
-    noFill();
-    beginShape();
-    for (const {
-        x,
-        y
-      } of this.body.vertices) {
-      curveVertex(x, y);
-    }
-    endShape(CLOSE);
-    pop();
-  }
-}
-
-//start sim after x time
 function setup() {
-  const cnv = createCanvas(windowWidth, Math.max(windowHeight, 300));
+  const canvas = createCanvas(500, 500);
 
-  engine = Engine.create();
-  world = engine.world;
+  // create an engine
+  const engine = Matter.Engine.create();
+  const world = engine.world;
 
-  const runner = Runner.create();
-  Runner.run(runner, engine);
-  //add ground
-  ground = Bodies.rectangle(width / 2, height, width, 50, {
-    isStatic: true
-  });
-  World.add(world, ground);
-  //add ceiling
-  ceiling = Bodies.rectangle(width / 2, 0, width, 50, {
-    isStatic: true
-  });
-  World.add(world, ceiling);
-  //add left wall
-  wallLeft = Bodies.rectangle(0, height / 2, 50, height, {
-    isStatic: true
-  });
-  World.add(world, wallLeft);
-  //add right wall
-  wallRight = Bodies.rectangle(width, height / 2, 50, height, {
-    isStatic: true
-  });
-  World.add(world, wallRight);
-  //create x bodies
-  for (let i = 0; i < 4; i++) {
-    blobs.push(new Blob(random(50, width - 100), random(50, height - 100)));
-  }
+  // config wrap area
+  const wrap = {
+    min: { x: 0, y: 0 },
+    max: { x: width, y: height }
+  };
+
+  board = loadImage('board.png');
+
+  blackrookSprite = loadImage('./pieces/blackrook.png');
+  blackknightSprite = loadImage('./pieces/blackknight.png');
+  blackbishopSprite = loadImage('./pieces/blackbishop.png');
+  blackqueenSprite = loadImage('./pieces/blackqueen.png');
+  blackkingSprite = loadImage('./pieces/blackking.png');
+  blackpawnSprite = loadImage('./pieces/blackpawn.png');
+
+
+  whiterookSprite = loadImage('./pieces/whiterook.png');
+  whiteknightSprite = loadImage('./pieces/whiteknight.png');
+  whitebishopSprite = loadImage('./pieces/whitebishop.png');
+  whitequeenSprite = loadImage('./pieces/whitequeen.png');
+  whitekingSprite = loadImage('./pieces/whiteking.png');
+  whitepawnSprite = loadImage('./pieces/whitepawn.png');
+  
+  // create room
+
+  ground = new Block(world, { x: 400, y: height+2, w: 810, h: 2, color: 'black' }, {isStatic: true });
+  ceiling = new Block(world, { x: 400, y: -2, w: 810, h: 2, color: 'black' }, {isStatic: true });
+  lwall = new Block(world, { x: -2, y: 400, w: 2, h: 810, color: 'black' }, {isStatic: true });
+  rwall = new Block(world, { x: width+2, y: 400, w: 2, h: 810, color: 'black' }, {isStatic: true });
+
+
+  // create pieces
+
+  blackrook1 = new Ball(world,
+    { x: 29, y: 30, r: 23, image: blackrookSprite },
+    { isStatic: stat, restitution: .8, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackknight1 = new Ball(world,
+    { x: 92, y: 30, r: 23, image: blackknightSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackbishop1 = new Ball(world,
+    { x: 156.5, y: 30, r: 23, image: blackbishopSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackqueen = new Ball(world,
+    { x: 219.5, y: 30, r: 23, image: blackqueenSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackking = new Ball(world,
+    { x: 281, y: 30, r: 23, image: blackkingSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackbishop2 = new Ball(world,
+    { x: 344, y: 30, r: 23, image: blackbishopSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackknight2 = new Ball(world,
+    { x: 405, y: 30, r: 23, image: blackknightSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackrook2 = new Ball(world,
+    { x: 468, y: 30, r: 23, image: blackrookSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+
+  blackpawn1 = new Ball(world,
+    { x: 29.1, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0.8, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn2 = new Ball(world,
+    { x: 92, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn3 = new Ball(world,
+    { x: 156.6, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn4 = new Ball(world,
+    { x: 219.5, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn5 = new Ball(world,
+    { x: 281, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn6 = new Ball(world,
+    { x: 344, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn7 = new Ball(world,
+    { x: 405, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  blackpawn8 = new Ball(world,
+    { x: 468, y: 95, r: 23, image: blackpawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  
+
+
+
+  whiterook1 = new Ball(world,
+    { x: 29, y: 406, r: 23, image: whiterookSprite },
+    { isStatic: stat, restitution: .8, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whiteknight1 = new Ball(world,
+    { x: 92, y: 406, r: 23, image: whiteknightSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitebishop1 = new Ball(world,
+    { x: 156.5, y: 406, r: 23, image: whitebishopSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitequeen = new Ball(world,
+    { x: 219.5, y: 406, r: 23, image: whitequeenSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whiteking = new Ball(world,
+    { x: 281, y: 406, r: 23, image: whitekingSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitebishop2 = new Ball(world,
+    { x: 344, y: 406, r: 23, image: whitebishopSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whiteknight2 = new Ball(world,
+    { x: 405, y: 406, r: 23, image: whiteknightSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whiterook2 = new Ball(world,
+    { x: 468, y: 406, r: 23, image: whiterookSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+
+  whitepawn1 = new Ball(world,
+    { x: 29, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn2 = new Ball(world,
+    { x: 92, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn3 = new Ball(world,
+    { x: 156.5, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn4 = new Ball(world,
+    { x: 219.4, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn5 = new Ball(world,
+    { x: 281, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn6 = new Ball(world,
+    { x: 344, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn7 = new Ball(world,
+    { x: 405, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+  whitepawn8 = new Ball(world,
+    { x: 468.1, y: 469, r: 23, image: whitepawnSprite },
+    { isStatic: stat, restitution: 0, plugin: { wrap: wrap }, label: 'character'}
+  );
+ 
+
+  // setup mouse
+  mouse = new Mouse(engine, canvas);
+
+  // gravity
+  engine.world.gravity.y = grav;
+
+  // run the engine
+  Matter.Runner.run(engine);
 }
 
 function draw() {
-  background('#EEF2FD');
-  //show all bodies
-  for (var i = 0; i < blobs.length; i++) {
-    blobs[i].show();
-  }
+  background(board);
+
+  mouse.draw();
+
+  ground.draw();
+  ceiling.draw();
+  lwall.draw();
+  rwall.draw();
+
+  blackrook1.draw();
+  blackknight1.draw();
+  blackbishop1.draw();
+  blackqueen.draw();
+  blackking.draw();
+  blackbishop2.draw();
+  blackknight2.draw();
+  blackrook2.draw();
+  blackpawn1.draw();
+  blackpawn2.draw();
+  blackpawn3.draw();
+  blackpawn4.draw();
+  blackpawn5.draw();
+  blackpawn6.draw();
+  blackpawn7.draw();
+  blackpawn8.draw();
+
+
+  whiterook1.draw();
+  whiteknight1.draw();
+  whitebishop1.draw();
+  whitequeen.draw();
+  whiteking.draw();
+  whitebishop2.draw();
+  whiteknight2.draw();
+  whiterook2.draw();
+
+  whitepawn1.draw();
+  whitepawn2.draw();
+  whitepawn3.draw();
+  whitepawn4.draw();
+  whitepawn5.draw();
+  whitepawn6.draw();
+  whitepawn7.draw();
+  whitepawn8.draw();
+
+
+
 }
 
-function mouseClicked() {
-  blobs.push(new Blob(mouseX, mouseY));
-}
